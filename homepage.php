20aeeed8a40e41262ac0e637b1a3008e7ba45cac
Colin Patty <89837160+colintolleigh@users.noreply.github.com>
2022-01-20 20:20:14 +0100

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title>Patta's By Tom</title>
+    <link rel="stylesheet" href="./styleHomepage.css">
+
+</head>
+
+<body>
+
+<header>
+    <h1>
+        <img src="pattasbytomlogo.jpg" width="400" height="440">
+    </h1>
+</header>
+<div class="menubar">
+    <nav>
+        <div><a href="homepage.php">Home</a></div>
+        <div><a href="reservepage.php">Reserveren</a></div>
+        <div><a href="aboutPage.php">About</a></div>
+    </nav>
+</div>
+
+<main>
+
+    <section id="Home">
+        <h2>Welkom!</h2>
+        <p>Welkom op de webpagina van Patta's by Tom! Hier kunt u uw customized sneaker aanvraag maken en kunt u meer
+            info over het bedrijf weten. </p>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Viverra justo nec ultrices dui sapien eget mi proin. Fames ac turpis egestas maecenas.
+            Nec nam aliquam sem et tortor. Fusce id velit ut tortor pretium viverra suspendisse potenti.
+            Pellentesque id nibh tortor id aliquet. Enim sit amet venenatis urna cursus eget nunc scelerisque.
+            Id neque aliquam vestibulum morbi blandit cursus risus at ultrices. Faucibus purus in massa tempor nec
+            feugiat. Pretium fusce id velit ut. Tempor nec feugiat nisl pretium.
+
+            Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a. Id nibh tortor id aliquet lectus proin nibh.
+            Vestibulum mattis ullamcorper velit sed ullamcorper morbi. Habitasse platea dictumst vestibulum rhoncus est.
+            Facilisis magna etiam tempor orci eu lobortis elementum nibh tellus.
+            <br>
+            Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Sed velit dignissim sodales
+            ut eu sem integer.
+            <br>
+            Senectus et netus et malesuada fames ac. Netus et malesuada fames ac turpis egestas integer eget.
+            At risus viverra adipiscing at in tellus integer feugiat.
+        </p>
+    </section>
+</main>
+
+<footer>
+    <div>Links</div>
+    <br>
+    <a href = "https://www.instagram.com/pattas_by_tom/" title="Klik om onze Instagram pagina te bekijken" target="_blank">Instagram</a>
+    <br>
+    <br>
+    <a href = "loginpagereal.php">Log In</a>
+</footer>
+<!-- partial -->
+
+</body>
+</html>
