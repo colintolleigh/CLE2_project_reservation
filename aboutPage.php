0bff737cb2ff6f85611e35212ef86e38d37e4d52
Colin Patty <89837160+colintolleigh@users.noreply.github.com>
2022-01-21 14:13:25 +0100

    Add files via upload

---
+++ aboutPage.php
@@ -0,0 +1,82 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+    <meta charset="UTF-8">
+    <title>Patta's By Tom</title>
+    <link rel="stylesheet" href="./styleHomepage.css">
+
+</head>
+
+<body>
+
+<header>
+    <h1>
+        <img src="pattasbytomlogo.jpg" width="400" height="440">
+    </h1>
+</header>
+<div class="menubar">
+    <nav>
+        <div><a href="homepage.php">Home</a></div>
+        <div><a href="reservepage.php">Reserveren</a></div>
+        <div><a href="aboutPage.php">About</a></div>
+    </nav>
+</div>
+
+<main>
+    <section id="About">
+        <h1>About</h1>
+        <p>
+            Welkom op de about page. Hier staat kort wat achtergrond informatie beschreven over de oprichter van Patta's
+            By Tom en het begin van deze business.
+        </p>
+
+<section id="AboutText"
+        <p>
+            Patta's by Tom is een business die echt voor de sneaker community is. Wij gaan voor jou de mooiste
+            custom sneakers maken en we bieden ook restauraties en cleaning aan!
+            <br><br>
+
+            Patta's by Tom is opgericht door Tom Stoelinga, student aan de Willem de Kooning Academie en een grote sneakerhead.
+            Doordat hij een passie heeft voor sneakers en hij graag de wereld van sneaker customizen wilt ontdekken kwam hij met Patta's by Tom.
+            About me
+            <br>
+            Mijn passie voor sneakers ontstond toen ik mijn eerste paar jordans kocht op de middelbaren school.
+            Toen ik ze voor het eerst aan trok wist ik dat dit niet de laatste zouden zijn.
+            Nu 5 jaar verder heb ik een leuke collectie voor mezelf opgebouwd.
+            Ik ben altijd wel creatief geweest, ik tekende altijd vaak en een paar jaar geleden dacht ik bij mezelf hoe kan ik deze twee passies met elkaar verbinden.
+            En zo ben ik begonnen met het customizen van sneakers en kleding stukken.
+            Ik begon kleine opdrachtjes te doen voor vrienden en familie om mijn portofolio op te bouwen en nu heb ik een klein ateliertje in mijn huis.
+
+        </p>
+
+        <br>
+
+
+        <h4>
+
+        </h4>
+        <p>
+        </p>
+        <br>
+    </section>
+
+</main>
+
+<footer>
+    <div>Links</div>
+    <br>
+    <a href = "https://www.instagram.com/pattas_by_tom/" title="Klik om onze Instagram pagina te bekijken" target="_blank">Instagram</a>
+    <br>
+    <br>
+    <a href = "loginpagereal.php">Log In</a>
+</footer>
+<!-- partial -->
+
+</body>
+</html>
+
